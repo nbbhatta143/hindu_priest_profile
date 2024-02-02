b6381a467f391bd7671bc9c701a46db9e259dd95
nbbhatta143 <61528359+nbbhatta143@users.noreply.github.com>
2024-02-01 19:00:12 -0500

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from "./logo.svg";
-import "./App.css";
-import HinduPriestProfile from "./component/HinduPriestProfile ";
+import logo from "./logo.svg"
+import "./App.css"
+import HinduPriestProfile from "./component/HinduPriestProfile"
 
 function App() {
   return (
     <div>
       <HinduPriestProfile />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
